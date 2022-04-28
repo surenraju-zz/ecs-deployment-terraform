fcec30c56409e4eca257659b7c66980d95a25c95
Suren Raju <suren.1988@gmail.com>
2022-04-28 09:23:50 +0400

    Adding terraform deployment config

---
+++ prod/backend.tf
@@ -0,0 +1,7 @@
+terraform {
+  backend "s3" {
+    bucket = "data-platform-ecs-terraform-state"
+    key    = "prod/data-platform-ecs.tfstate"
+    region = "ap-south-1"
+  }
+}
---
+++ prod/terraform.tfvars
@@ -0,0 +1,5 @@
+# Metadata
+region      = "ap-south-1"
+project     = "data-platform" 
+environment = "prod" 
+owner       = "Suren Raju"
